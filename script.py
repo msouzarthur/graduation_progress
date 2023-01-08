7fec784a030563a5b2538d3c7a50246412e5abc0
Arthur Machado <arthur.souza@verdecard.com.br>
2023-01-08 20:17:08 -0300

    :boom: fix: conserto na interface grafica

--- script.py
+++ script.py
@@ -32,11 +32,13 @@ class CourseFrame(customtkinter.CTkFrame):
             btn_course_image = self.refresh_image
 
         self.label_course_img = customtkinter.CTkLabel(self, text="", image=self.book_image)
-        self.label_course_img.grid(row=0, column=2, padx=20, pady=60)
+        self.label_course_img.pack(padx=20, pady=60)
+        
         self.label_course_name = customtkinter.CTkLabel(self, text=course)
-        self.label_course_name.grid(row=1, column=2, padx=20, pady=10)
+        self.label_course_name.pack(padx=20, pady=10)
+        
         self.button_course = customtkinter.CTkButton(self, text=msg_2,  compound="left", image=btn_course_image)
-        self.button_course.grid(row=2, column=2, padx=20, pady=10)
+        self.button_course.pack(padx=20, pady=10)
 
 class LoginFrame(customtkinter.CTkFrame):
 
@@ -50,9 +52,6 @@ class LoginFrame(customtkinter.CTkFrame):
         
         self.header_name = header_name
 
-        self.header = customtkinter.CTkLabel(self, text=self.header_name)
-        self.header.grid(row=0, column=0, padx=10, pady=10)
-
         if studentName == 'CARREGUE SEU HISTÓRICO':
             msg_1 = "CARREGAR HISTÓRICO"
             btn_student_image = self.download_image
@@ -61,11 +60,13 @@ class LoginFrame(customtkinter.CTkFrame):
             btn_student_image = self.refresh_image
 
         self.label_student_img = customtkinter.CTkLabel(self, text="", image=self.profile_image)
-        self.label_student_img.grid(row=0, column=0, padx=20, pady=60)
+        self.label_student_img.pack(padx=20, pady=60)
+        
         self.label_student_name = customtkinter.CTkLabel(self, text=studentName)
-        self.label_student_name.grid(row=1, column=0, padx=20, pady=10)
+        self.label_student_name.pack(padx=20, pady=10)
+        
         self.button_student = customtkinter.CTkButton(self, text=msg_1, compound='left', image=btn_student_image)
-        self.button_student.grid(row=2, column=0, padx=20, pady=10)
+        self.button_student.pack(padx=20, pady=10)
    
 class NavigationFrame(customtkinter.CTkFrame):
 
@@ -80,45 +81,35 @@ class NavigationFrame(customtkinter.CTkFrame):
         
         self.header_name = header_name
 
-        self.header = customtkinter.CTkLabel(self, text=self.header_name)
-        self.header.grid(row=0, column=0, padx=10, pady=10)
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
-        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(10, weight=1)
-
-        self.home_btn = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
-                                                fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                image=self.home_image_small, anchor="w"
-        )
-        self.home_btn.grid(row=3, column=0, sticky="ew")
-
-        self.profile_btn = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      anchor="w" , image=self.profile_image_small
-        )
-        self.profile_btn.grid(row=4, column=0, sticky="ew")
-
-        self.course_btn = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      anchor="w", image=self.book_image_small
-        )
-        self.course_btn.grid(row=5, column=0, sticky="ew")
+        
+        self.home_btn = customtkinter.CTkButton(self, corner_radius=0, height=40, border_spacing=20, text="MENU",
+                                                fg_color="transparent", text_color=("gray10", "gray90"), 
+                                                hover_color=("gray70", "gray30"), image=self.home_image_small, anchor="w")
+        self.home_btn.pack()
+        
+        self.profile_btn = customtkinter.CTkButton(self, corner_radius=0, height=40, border_spacing=20, text="ALUNO",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), 
+                                                      hover_color=("gray70", "gray30"), anchor="w", image=self.profile_image_small)
+        self.profile_btn.pack()
+        
+        self.course_btn = customtkinter.CTkButton(self, corner_radius=0, height=40, border_spacing=20, text="CURSO",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), 
+                                                      hover_color=("gray70", "gray30"), anchor="w", image=self.book_image_small)
+        self.course_btn.pack()
 
 class App(customtkinter.CTk):
 
     def __init__(self):
+
         super().__init__()
 
         self.title("graduation progress")
         self.geometry("1000x600")
 
-        # configure grid layout (4x4)
-        # self.grid_columnconfigure(1, weight=1)
-        # self.grid_columnconfigure((2, 3), weight=0)
-        # self.grid_rowconfigure((0, 1, 2), weight=1)
-        
-        # self.grid_rowconfigure(5, weight=1)
-        # self.grid_columnconfigure(5, weight=1)
+        # configure grid layout      
+        Grid.columnconfigure(self, (0,1,2,3,4),weight=1)
+        Grid.rowconfigure(self, (0,1), weight=1)
 
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), r"img")
         self.home_image_small       = customtkinter.CTkImage(Image.open(os.path.join(image_path, "home.png")), size=(20, 20))
@@ -131,62 +122,56 @@ class App(customtkinter.CTk):
 
         # create home frame
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid_columnconfigure(0, weight=1)
-     
+ 
+        # create navigation frame
+        self.navigation_frame = NavigationFrame(self, header_name="SIDEMENU")
+        self.navigation_frame.grid(row=0, column=0, sticky="NSW", rowspan=2)
+
         # create login frame
-        # self.login_frame = LoginFrame(self, header_name="USUÁRIO")
-        # self.login_frame.grid(row=1, column=0, padx=0, pady=0, sticky="s")
-
-        # # create course frame
-        # self.login_frame = CourseFrame(self, header_name="CURSO")
-        # self.login_frame.grid(row=0, column=4, padx=40, pady=80, sticky="e")
-
-        # # crate navigation frame
-        # self.navigation_frame = NavigationFrame(self, header_name="SIDEMENU")
-        # self.navigation_frame.grid(row=0, column=0, padx=0, pady=0, sticky="nw")
-        
-        self.home_frame_button = customtkinter.CTkButton(self.home_frame, text="CONTINUAR")
-        self.home_frame_button.grid(row=0, column=0, padx=30, pady=80)
-
-        # create profile frame
+        self.login_frame = LoginFrame(self, header_name="USUÁRIO")
+        self.login_frame.grid(row=0, column=1, padx=0, pady=0)
 
         # create course frame
-
-        # create statics frame
+        self.login_frame = CourseFrame(self, header_name="CURSO")
+        self.login_frame.grid(row=0, column=3, padx=0, pady=0)
+        
+        # create button 
+        self.home_frame_button = customtkinter.CTkButton(self, text="CONTINUAR")
+        self.home_frame_button.grid(row=1, column=2, padx=0, pady=0, sticky="W")
+        
+        #  create statics frame
         
         # select default frame
         self.select_frame_by_name("home")
 
-        # navigation frame 
-        
-
     def select_frame_by_name(self, name):
 
-        if studentName == 'CARREGUE SEU HISTÓRICO' or graduationId == 0:
-            self.home_frame_button.configure(fg_color="transparent")
-    #     self.home_frame_button_course.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-        
+        if studentName == 'CARREGUE SEU HISTÓRICO':
+            self.home_frame_button.configure(fg_color="transparent", text="CARREGUE SEU HISTÓRICO")
+        if graduationId == 0:
+            self.home_frame_button.configure(fg_color="transparent", text="SELECIONE SEU CURSO")
+
         # show selected frame
         if name == "home":
             self.home_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.home_frame.grid_forget()
-
-         
+    
 
 
 def load_courses():
-    # Dicionário de cursos
+    # courses dictionary
     print("carregando cursos", end=' ')
     file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), r"docs/courses_file.csv")
     try:
         with open(file_path, 'r') as f:
             reader = csv.reader(f)
             coursesDict = dict((rows[0],rows[1]) for rows in reader)
-        print(u"\u2713")
+        # print(u"\u2713")  charmap error
         return coursesDict
     except:
-        print(u"\u2717")
+        pass
+        # print(u"\u2717") charmap error
 
 def optionmenu_callback(choice):
     print("clicked:", choice)
@@ -262,19 +247,19 @@ def extract_disciplines(target):
 def load_student_file():
     global studentName, graduationId
     studentFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), r"docs/historico_aluno.pdf")
-    print('carregando histórico do aluno', end=' ')    
+    print('carregando historico do aluno', end=' ')    
     try:
         reader = PdfReader(studentFile)
         studentHist = extract_content(reader)
         studentName = extract_name(studentHist)
         graduationId = extract_id(studentHist)
         studentHist = extract_disciplines(studentHist)
-        print(u"\u2713")
+        # print(u"\u2713") charmap error
         return studentHist
     except:
-        print(u"\u2717")
+        pass
+        # print(u"\u2717") charmap error
     
-
 if __name__=='__main__':
 
     studentName = 'CARREGUE SEU HISTÓRICO'
